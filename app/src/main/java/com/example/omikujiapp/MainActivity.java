ecc2f226819ddadd0a125c44c0f8ba9465959b87
西潟アイ子 <lovebernis1944@gmail.com>
2024-11-06 21:40:31 +0900

    画面遷移実装完了

--- app/src/main/java/com/example/omikujiapp/MainActivity.java
+++ app/src/main/java/com/example/omikujiapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.omikujiapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +25,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
 
-
         });
+        //開始ボタンを読み込む
+        startBtn = findViewById(R.id.start_buttom_text);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //次の画面に遷移する
+                //Context: ApplocationContext, ActivityContextどちらか指定
+                //行く先のクラスを指定
+                Intent intent = new Intent(MainActivity.this,ResultActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
